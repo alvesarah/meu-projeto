1da7027d17d8d412be8d98205ffc5b558ab846f2
alvesarah <sarah.alves8@hotmail.com>
2022-04-10 17:38:19 -0300

    Testando funções possiveis com JSX no App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import './App.css';
 
 function App() {
+  const nome = "Sarah";
+  
+  const nomeMaiusculo = nome.toUpperCase();
+
+  function soma(a,b){
+    return a + b;
+  }
+
+  const url = "https://via.placeholder.com/150";
+
   return (
     <div className="App">
       <h1>Ola react!</h1>
+      <p>Ola, {nomeMaiusculo}</p>
+      <p>Soma {soma(3, 4)}</p>
+      <img src={url} alt="Minha imagem"/>
     </div>
   );
 }
